217865079b52f2b73dec94d601fe70529c5cfb42
yousuf812 <yousuf.me.bd@gmail.com>
2023-11-24 18:34:41 +0600

    finished

--- src/Component/Countries/Country.js
+++ src/Component/Countries/Country.js
@@ -11,8 +11,17 @@ const Country = (country) => {
            <p>Population:{country.country.population}</p>
            <p>{country.country.continents[0]}</p>
            <img src={country.country.flags.png} alt={country.country.name.common} width="200px" height="200px"/>
+           <button className='btn btn-danger' onClick={()=>displayDetails(cca2)}>SHOW DETAILS</button>
         </div>
     );
 };
+
+let displayDetails =(code) => {
+    //console.log('code');
+    let url=`https://restcountries.com/v3.1/alpha/${code}`;
+    fetch(url)
+    .then((response) => response.json())
+    .then(data=>console.log(country.length))
+
 
 export default Country;
